7314bbdada346ea6c46ad7fbff4d0c21ccc3fc80
IcakaXXD <131612221+IcakaXXD@users.noreply.github.com>
2023-04-23 21:41:17 +0300

    Add files via upload

---
+++ BusinessLayer/Bottle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    class Bottle
+    {
+        [Key]
+        public int Id { get; set; }
+       
+        public char Type { get; set; }
+        public char Size { get; set; }
+        [Range(0,100000,ErrorMessage ="Price must be greater than -1.")]
+        public decimal Price { get; set; }
+        public User User { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        private Bottle()
+        {
+
+        }
+        public Bottle(char type, char size,decimal price,int userId)
+        {
+            this.Type = type;
+            Size = size;
+            Price = price;
+            UserId = userId;
+        }
+    }
+}
---
+++ BusinessLayer/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Range(5,30,ErrorMessage ="Name must not be smaller than 5 symbols and larger than 30 symbols.")]
+        public string Name { get; set; }
+
+        [Range(0,10000000,ErrorMessage ="Money cannot be less than zero.")]
+        public decimal Money { get; set; }
+
+        private List<Bottle> bottles;
+
+        private User()
+        {
+            bottles = new List<Bottle>();
+        }
+        public User(string name)
+        {
+            this.Name = name;
+            Money = 0;
+            bottles = new List<Bottle>();
+        }
+
+    }
+}
